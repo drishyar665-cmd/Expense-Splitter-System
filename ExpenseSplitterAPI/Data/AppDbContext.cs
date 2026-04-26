b448679b154e87d03a37175498027ceb959b665e
sandravarghese01 <sandravarghese2718@gmail.com>
2026-04-26 19:01:52 +0530

    AppDbContext.cs

--- ExpenseSplitterAPI/Data/AppDbContext.cs
+++ ExpenseSplitterAPI/Data/AppDbContext.cs
@@ -7,7 +7,7 @@ namespace ExpenseSplitterAPI.Data
     {
         public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
 
-        // These names must exactly match Disha's table names in ExpenseDB
+        
         public DbSet<User> Users { get; set; }
         public DbSet<Group> Groups { get; set; }
         public DbSet<GroupMember> GroupMembers { get; set; }
@@ -16,11 +16,11 @@ namespace ExpenseSplitterAPI.Data
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
-            // GroupMembers has a composite key (GroupID + UserID together = unique row)
+            
             modelBuilder.Entity<GroupMember>()
                 .HasKey(gm => new { gm.GroupID, gm.UserID });
 
-            // Tell EF the primary key column names (they use ID not Id)
+           
             modelBuilder.Entity<User>()
                 .HasKey(u => u.UserID);
 
